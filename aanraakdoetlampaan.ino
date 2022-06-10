786dc702b0bb2ba473ed2fbe4fd444aedab79efd
protected22 <simonjanse22@gmail.com>
2022-06-10 09:42:09 +0200

    day4
    day4

--- aanraakdoetlampaan.ino
+++ aanraakdoetlampaan.ino
@@ -1,5 +1,6 @@
 #include <CapacitiveSensor.h>
 CapacitiveSensor touchSwitch = CapacitiveSensor(4, 2);
+
 int LEDpin = 11;
 String Touch = "Touch"; 
 String Ready = "Ready!"; 
@@ -13,22 +14,24 @@ pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
 }
 void loop(){
 long val = touchSwitch.capacitiveSensor(100);
-if (val > 3000) {
+if (val > 2000) {
 digitalWrite(LEDpin, HIGH);
-Serial.println(Touch); // print sensor output
+//Serial.println(Touch); // print sensor output
 tone(buzzer, 1100); // Send 1KHz sound signal...
 } else {
 digitalWrite(LEDpin, LOW);
-Serial.println(Ready); // print sensor output
+//Serial.println(Ready); // print sensor output
 noTone(buzzer);     // Stop sound...
 }
 
+
+tone(buzzer, 1100); // Send 1KHz sound signal...
 
 
 
 
 //debuging
-//Serial.println(val); // print sensor output
+Serial.println(val); // print sensor output
 //tone(buzzer, 1100); // Send 1KHz sound signal...
 //delay(500);
 //tone(buzzer, 800); // Send 1KHz sound signal...
